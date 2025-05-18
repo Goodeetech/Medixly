2427c8be41bc7f20ae010db53f3daea6d29cffa9
Goodeetech <ayomidegoodee@gmail.com>
2025-05-18 18:06:39 +0100

    shadcn implementation

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="text-[#0F172A]">
+    <div className="">
       <h1>let build a remainder app</h1>
+      <Button>Login</Button>
     </div>
   );
 }
