5d31fb3b4c62615a00ace88513bf6ee8173796a6
Goodeetech <ayomidegoodee@gmail.com>
2025-05-22 19:59:58 +0100

    implemented the Quiz preview page

--- app/(main)/dashboard/create-quiz/_component/QuizCart.tsx
+++ app/(main)/dashboard/create-quiz/_component/QuizCart.tsx
@@ -1,8 +1,19 @@
 "use client";
 import React, { useEffect, useRef } from "react";
 import axios from "axios";
-import { Loader2Icon } from "lucide-react";
+import {
+  ArrowRight,
+  Heart,
+  Loader2Icon,
+  Settings,
+  Settings2Icon,
+  Share,
+  Share2Icon,
+  TimerIcon,
+} from "lucide-react";
 import Image from "next/image";
+import { useUser } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
 
 interface QuizCartProps {
   formData: any; // Replace 'any' with the actual type if known, e.g., { question: string; answer: string }
@@ -12,6 +23,7 @@ const QuizCart: React.FC<QuizCartProps> = ({ formData }) => {
   const [questionList, setQuestionList] = React.useState<any[]>([]); // Replace 'any' with the actual type if known
   const [loading, setLoading] = React.useState(false);
   const hasFetchedRef = useRef(false);
+  const { user } = useUser();
   useEffect(() => {
     if (formData && !hasFetchedRef.current) {
       hasFetchedRef.current = true;
@@ -86,9 +98,69 @@ const QuizCart: React.FC<QuizCartProps> = ({ formData }) => {
               </div>
             </div>
             <div>
-              <div className="px-6 my-2 bg-[#272A30] rounded-lg text-gray-200 py-4">
-                <h3 className="uppercase"> {formData?.QuizTitle}</h3>
-                <h4 className="text-sm">{formData?.QuizDescription}</h4>
+              <div className="px-4 my-2 bg-[#34363b] rounded-lg text-gray-200 shadow-2xl flex gap-10 items-center  py-4">
+                <div className="flex justify-center items-center h-full shadow-xl bg-[#1f2020] rounded">
+                  <Image
+                    src={"/quizimage.png"}
+                    alt="quiz"
+                    height={200}
+                    width={200}
+                    className="h-full"
+                  />
+                </div>
+                <div>
+                  <h3 className="text-2xl capitalize font-semibold">
+                    {formData?.QuizTitle}
+                  </h3>
+                  <h5 className="font-light text-xs">
+                    {formData?.QuizDescription}
+                  </h5>
+
+                  <div className="flex mt-6 gap-2 items-center ">
+                    {/* <div className=" rounded-full bg-amber-200 h-8 w-8 "></div> */}
+                    <Image
+                      src={formData?.imageUrl || "/quizimage.png"}
+                      alt="imagee"
+                      height={20}
+                      width={20}
+                    />
+                    <div className="flex flex-col ">
+                      <h3 className="text-xs font-extralight">Created by</h3>
+                      <h2 className="text-xs">{user?.fullName}</h2>
+                    </div>
+                  </div>
+                  <div className="grid grid-cols-2 ">
+                    <div className="flex items-center gap-2 mt-4">
+                      <div className="bg-black p-1 rounded-full inline-flex items-center justify-center">
+                        <Settings color="#308579" size={16} />
+                      </div>
+                      <div className="flex flex-col">
+                        <h3 className="text-xs font-extralight">Difficulty</h3>
+                        <h2 className="text-xs">{formData?.DifficultyLevel}</h2>
+                      </div>
+                    </div>
+                    <div className="flex items-center gap-2 mt-4">
+                      <div className="bg-black   p-1 rounded-full">
+                        <TimerIcon color="#308579" size={16} />
+                      </div>
+                      <div className="flex flex-col">
+                        <h3 className="text-xs font-extralight">Time Limit</h3>
+                        <h2 className="text-xs">{formData?.TimeLimit}</h2>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="mt-6 flex items-center gap-6">
+                    <Button className="!px-8 !py-2 tracking-wider">
+                      Start Quiz <ArrowRight size={32} />
+                    </Button>
+                    <div className="p-2 rounded-full outline-gray-400 outline flex items-center ">
+                      <Share2Icon size={16} />
+                    </div>
+                    <div className="p-2 rounded-full outline-gray-400 outline flex items-center ">
+                      <Heart size={16} />
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
