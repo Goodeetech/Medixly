b57f0909886ed68594aecf0ad5e1868d0d3b8fd1
Goodeetech <ayomidegoodee@gmail.com>
2025-06-02 19:38:20 +0100

    Added some changes for mobile

--- components/layers/Dashboard.tsx
+++ components/layers/Dashboard.tsx
@@ -64,16 +64,16 @@ const Dashboard = () => {
 
   return (
     <div className="bg-gray-100 my-4 mx-6 py-6 px-4 rounded-lg">
-      <div className="flex justify-between items-center">
+      <div className="flex flex-col gap-3 md:flex-row justify-between items-center">
         <div>
           <h1 className="text-3xl font-semibold text-gray-800">Dashboard</h1>
           <h4 className="text-[#436964] text-sm mt-2">
             Track progress. Master topics. Stay sharp.
           </h4>
         </div>
-        <div className="flex items-center gap-4">
+        <div className="flex items-center  gap-4">
           <Link href="/dashboard/create-quiz">
-            <Button className=" bg-gradient-to-b from-[#4382BE] to-[#45B4A6] px-8 py-5 tracking-wide rounded-full cursor-pointer  flex items-center gap-x-2 hover:bg-[#366861] transition-all duration-400 ">
+            <Button className=" bg-gradient-to-b from-[#4382BE] to-[#45B4A6] px-8 py-5 tracking-wide rounded-full cursor-pointer  flex items-center  gap-x-2 hover:bg-[#366861] transition-all duration-400 ">
               <Plus size={32} />
               Create Quiz
             </Button>
@@ -87,7 +87,7 @@ const Dashboard = () => {
           </Button>
         </div>
       </div>
-      <div className="grid lg:grid-cols-3 grid-cols-2 mt-4 gap-4">
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 grid-cols-1 mt-4 gap-4">
         <QuizCard
           title="Total Quizzes"
           value={24}
