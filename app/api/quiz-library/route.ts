29003fb1d4aa7ba4df1a0c8ab79fec33fdda658c
Goodeetech <ayomidegoodee@gmail.com>
2025-06-22 09:42:51 +0100

    changed the ai model and improved the prompts

--- app/api/quiz-library/route.ts
+++ app/api/quiz-library/route.ts
@@ -23,7 +23,7 @@ export async function POST(req: Request) {
     try {
       console.time("🔁 GROQ Completion");
       const completion = await openai.chat.completions.create({
-        model: "llama3-8b-8192",
+        model: "llama3-70b-8192",
         messages: [{ role: "user", content: FinalPrompt }],
       });
       console.timeEnd("🔁 GROQ Completion");
