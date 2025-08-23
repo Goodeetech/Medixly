2de900e018beca006af1ce691a02111951b49e6e
Goodeetech <ayomidegoodee@gmail.com>
2025-08-23 19:23:29 +0100

    lets see

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -7,22 +7,31 @@ import NavBar from "@/components/layers/NavBar";
 
 export default function DashboardLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <div>
       <DashBoardProvider>
+        {/* Provider should always wrap everything */}
         <SidebarProvider>
-          <AppSidebar />
+          {/* Sidebar itself can be memoized so it doesn’t remount unnecessarily */}
+          <StableSidebar />
+
           <main className="w-full">
             <SidebarTrigger />
             <NavBar />
             {children}
           </main>
+
           <Toaster />
         </SidebarProvider>
       </DashBoardProvider>
     </div>
   );
 }
+
+/** 👇 memoized AppSidebar so it doesn’t remount every route change */
+const StableSidebar = React.memo(function StableSidebar() {
+  return <AppSidebar />;
+});
